a72d5b396988431f696c0ad500b237f62d731c29
Mads Storgaard-Nielsen <s180076@win.dtu.dk>
2020-10-17 18:01:30 +0200

    -

--- app/src/main/java/dk/gruppea3moro/moroa3/MainActivity.java
+++ app/src/main/java/dk/gruppea3moro/moroa3/MainActivity.java
@@ -1,15 +1,21 @@
 package dk.gruppea3moro.moroa3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Fragment bottomBarFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Test
+
+        bottomBarFragment = new bottomBarFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.bottomBarFL, bottomBarFragment)  // tom container i layout
+                .commit();
     }
 }
---
+++ app/src/main/java/dk/gruppea3moro/moroa3/bottomBarFragment.java
@@ -0,0 +1,19 @@
+package dk.gruppea3moro.moroa3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class bottomBarFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom_bar, container, false);
+    }
+}
