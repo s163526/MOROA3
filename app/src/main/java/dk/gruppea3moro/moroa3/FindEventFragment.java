fea79788da323e81211a9fd55015a31f6ac8445b
peterrevsbech <peter.revsbech@gmail.com>
2020-10-17 19:57:11 +0200

    Har flyttet OnClick() -metoderne ind i FrontpageFragment så knapperne på forsiden virker

--- app/src/main/java/dk/gruppea3moro/moroa3/FindEventFragment.java
+++ app/src/main/java/dk/gruppea3moro/moroa3/FindEventFragment.java
@@ -12,37 +12,16 @@ import android.widget.Button;
 
 //TODO viewpager
 public class FindEventFragment extends Fragment implements View.OnClickListener {
-    Button rightNowButton, findEventButton;
-    Fragment findEventFragment;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.fragment_find_event, container, false);
-
-        //Buttons
-        rightNowButton = v.findViewById(R.id.rightNowButton);
-        findEventButton = v.findViewById(R.id.findEventButton);
-        findEventButton.setOnClickListener(this);
-        rightNowButton.setOnClickListener(this);
-
-        return v;
+        View root = inflater.inflate(R.layout.fragment_find_event, container, false);
+        return root;
     }
 
     @Override
     public void onClick(View v) {
-        if (v == findEventButton) {
-            findEventFragment = new ShowResultFragment();
-            getFragmentManager().beginTransaction()
-                    .add(R.id.findEventFragment, findEventFragment)  // tom container i layout
-                    .commit();
-        }
 
-        if (v == rightNowButton) {
-            findEventFragment = new ShowResultFragment();
-            getFragmentManager().beginTransaction()
-                    .add(R.id.mainFL, findEventFragment)  // tom container i layout
-                    .commit();
-        }
     }
 }
--- app/src/main/java/dk/gruppea3moro/moroa3/FrontpageFragment.java
+++ app/src/main/java/dk/gruppea3moro/moroa3/FrontpageFragment.java
@@ -7,13 +7,44 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class FrontpageFragment extends Fragment {
+public class FrontpageFragment extends Fragment implements View.OnClickListener {
+
+    Button rightNowButton, findEventButton;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frontpage, container, false);
+        View root = inflater.inflate(R.layout.fragment_frontpage, container, false);
+
+        //Buttons
+        rightNowButton = root.findViewById(R.id.rightNowButton);
+        findEventButton = root.findViewById(R.id.findEventButton);
+        findEventButton.setOnClickListener(this);
+        rightNowButton.setOnClickListener(this);
+
+
+
+        return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == findEventButton) {
+            Fragment findEventFragment = new FindEventFragment();
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.mainFL, findEventFragment)  // tom container i layout
+                    .addToBackStack(null).commit();
+        }
+
+        else if (v == rightNowButton) {
+            Fragment findEventFragment = new ShowResultFragment();
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.mainFL, findEventFragment)  // tom container i layout
+                    .addToBackStack(null).commit();
+        }
     }
 }
