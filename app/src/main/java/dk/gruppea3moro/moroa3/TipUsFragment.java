576accd9c505f0e63b81205bca722c9713668f46
Christian <christiankyed@hotmail.com>
2020-10-27 11:17:56 +0100

    Påbegyndt opsætning af "Tip os"

--- app/src/main/java/dk/gruppea3moro/moroa3/TipUsFragment.java
+++ app/src/main/java/dk/gruppea3moro/moroa3/TipUsFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class TipUsFragment extends Fragment {
@@ -14,7 +16,33 @@ public class TipUsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_tip_os, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tip_os, container, false);
+
+        //Inputs data into the 3 Spinners
+        //TODO Som det er nu vil man kunne vælge d. 31 februar. Der bliver ikke taget højde fro hvilken måned du har valgt.
+        for (int i = 0; i < 3; i++) {
+            int n = R.id.dateDay;
+            int t = R.array.date;
+            if(i==1){
+                n=R.id.dateMonth;
+                t = R.array.month;
+            }else if(i==2){
+                n=R.id.dateYear;
+                t = R.array.year;
+            }
+            Spinner spinner = (Spinner)root.findViewById(n);
+// Create an ArrayAdapter using the string array and a default spinner layout
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                    t, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+            spinner.setAdapter(adapter);
+        }
+
+
+        return root;
     }
+
 }
