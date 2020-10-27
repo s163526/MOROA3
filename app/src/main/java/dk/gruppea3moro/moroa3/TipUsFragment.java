105154429d4fe3a63dc7d95a0757ef4d4685cd44
Christian <christiankyed@hotmail.com>
2020-10-27 11:48:35 +0100

    Opsætning af TIP OS er videreudviklet

--- app/src/main/java/dk/gruppea3moro/moroa3/TipUsFragment.java
+++ app/src/main/java/dk/gruppea3moro/moroa3/TipUsFragment.java
@@ -2,16 +2,23 @@ package dk.gruppea3moro.moroa3;
 
 import android.os.Bundle;
 
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
 
-public class TipUsFragment extends Fragment {
+
+public class TipUsFragment extends Fragment implements View.OnClickListener {
+    Button chooseDate, saveEvent;
+
+    //TODO Knappen som skal gemme variablerne mangler at blive implementeret
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,6 +26,15 @@ public class TipUsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_tip_os, container, false);
         // Inflate the layout for this fragment
 
+        chooseDate = root.findViewById(R.id.eventChooseDate);
+        chooseDate.setOnClickListener(this);
+
+
+
+
+
+        /*
+        TODO Skal muligvis bare slettes afhænig af hvad MORO gerne vil have af valg af dato metode
         //Inputs data into the 3 Spinners
         //TODO Som det er nu vil man kunne vælge d. 31 februar. Der bliver ikke taget højde fro hvilken måned du har valgt.
         for (int i = 0; i < 3; i++) {
@@ -40,9 +56,16 @@ public class TipUsFragment extends Fragment {
 // Apply the adapter to the spinner
             spinner.setAdapter(adapter);
         }
-
+*/
 
         return root;
     }
-
+    @Override
+    public void onClick(View v){
+        if(v==chooseDate) {
+            MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
+            MaterialDatePicker<Long> picker = builder.build();
+            picker.show(getFragmentManager(), picker.toString());
+        }
+    }
 }
