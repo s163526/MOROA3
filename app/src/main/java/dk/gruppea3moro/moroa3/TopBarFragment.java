c053f2e6de3c61b9c5385006a7bfea26b3d5285f
Mads Storgaard-Nielsen <s180076@win.dtu.dk>
2020-10-27 10:37:54 +0100

    started implementing top search bar

--- app/src/main/java/dk/gruppea3moro/moroa3/TopBarFragment.java
+++ app/src/main/java/dk/gruppea3moro/moroa3/TopBarFragment.java
@@ -1,17 +1,23 @@
 package dk.gruppea3moro.moroa3;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class TopBarFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_top_bar, container, false);
+
         return root;
     }
 }
