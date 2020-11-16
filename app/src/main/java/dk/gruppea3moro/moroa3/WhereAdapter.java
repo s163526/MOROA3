0c042ad43c0c41c51b1dbb7d7fc988838e1a32de
Sebastian <s163526@student.dtu.dk>
2020-11-16 11:21:52 +0100

    Startet på GridView

---
+++ app/src/main/java/dk/gruppea3moro/moroa3/WhereAdapter.java
@@ -0,0 +1,41 @@
+package dk.gruppea3moro.moroa3;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class WhereAdapter extends BaseAdapter {
+
+    private final Context mContext;
+    private final String places[] ={"Nørrebro", "Islandsbrygge", "Indre By", "Østerbro", "Nordvest", "Valby", "Brønshøj & Husum", "Amager", "Vesterbro", "Vanløse", "Christianshavn", "Refshaleøen"} ;
+
+
+    // 1
+    public WhereAdapter(Context context, String[] places) {
+        this.mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView dummyTextView = new TextView(mContext);
+        dummyTextView.setText(String.valueOf(position));
+        return dummyTextView;
+    }
+}
