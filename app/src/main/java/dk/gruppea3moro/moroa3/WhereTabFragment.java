391f632869342e3e25cd33536c833962925b1102
Sebastian <s163526@student.dtu.dk>
2020-11-17 08:01:16 +0100

    Startet på GridView

--- app/src/main/java/dk/gruppea3moro/moroa3/WhereTabFragment.java
+++ app/src/main/java/dk/gruppea3moro/moroa3/WhereTabFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 import androidx.fragment.app.Fragment;
 
-public class WhereTabFragment extends Fragment {
+public class WhereTabFragment extends Fragment implements AdapterView.OnItemClickListener {
 
     private final String places[] ={"Nørrebro", "Islandsbrygge", "Indre By", "Østerbro", "Nordvest", "Valby", "Brønshøj & Husum", "Amager", "Vesterbro", "Vanløse", "Christianshavn", "Refshaleøen"} ;
     @Override
@@ -16,13 +18,24 @@ public class WhereTabFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rv = inflater.inflate(R.layout.fragment_where_tab, container, false);
-        GridView gridView = rv.findViewById(R.id.gridview);
-        WhereAdapter whereAdapter = new WhereAdapter(getContext(), places);
-        gridView.setAdapter(whereAdapter);
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), R.layout.fragment_where_tab, R.id.listeelem_overskrift, places);
+
+        GridView gridView = new GridView(getContext());
+        gridView.setOnItemClickListener(this);
+        gridView.setNumColumns(GridView.AUTO_FIT);
+
+        //gridView.setAdapter(adapter);
+
+        //setContentView(gridView);
 
 
 
         return rv;
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
 }
