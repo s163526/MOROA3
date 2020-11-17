43876976de65780e32d9c682b1ae4b01ca809e86
Sebastian <s163526@student.dtu.dk>
2020-11-17 08:18:20 +0100

    Startet på GridView

--- app/src/main/java/dk/gruppea3moro/moroa3/WhereTabFragment.java
+++ app/src/main/java/dk/gruppea3moro/moroa3/WhereTabFragment.java
@@ -9,17 +9,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-public class WhereTabFragment extends Fragment {
+public class WhereTabFragment extends Fragment  {
+
+    private final String places[] ={"Nørrebro", "Islandsbrygge", "Indre By", "Østerbro", "Nordvest", "Valby", "Brønshøj & Husum", "Amager", "Vesterbro", "Vanløse", "Christianshavn", "Refshaleøen"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), R.layout.fragment_where_tab, R.id.listeelem_overskrift, places);
+
+        GridView gridView = new GridView(getContext());
+        gridView.setOnItemClickListener(this);
+        gridView.setNumColumns(GridView.AUTO_FIT);
+
+
         return inflater.inflate(R.layout.fragment_where_tab, container, false);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 
 }
